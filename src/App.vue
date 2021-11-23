88dbfdbc74ee877e3b381057c9a1d7ae9278b60a
LarisaGimbu <gimbularisa@gmail.com>
2021-11-23 16:03:58 +0100

    Title

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <Title />
+    <main>Main</main>
   </div>
 </template>
 
 <script>
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Title
   }
 }
 </script>
